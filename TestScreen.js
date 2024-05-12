0411e36b35baa202074402a1a14af1ddadbc5528
Kyato264 <158293638+Kyato264@users.noreply.github.com>
2024-05-12 02:58:04 -0400

    Delete TestScreen.js
    not in use

--- TestScreen.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-export default function Testscreen({ navigation }) {
-return (
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text>Test Screen</Text>
-<Button title="Go to Login" onPress={() => navigation.navigate('Login')} /></View>
-);
-}
