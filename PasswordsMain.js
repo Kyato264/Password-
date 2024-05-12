598d975e746e7404ca6cf3c72b8f65589770e180
Kyato264 <158293638+Kyato264@users.noreply.github.com>
2024-05-12 02:55:55 -0400

    Add files via upload
    Removed login button

--- PasswordsMain.js
+++ PasswordsMain.js
@@ -3,7 +3,6 @@ export default function PasswordsMain({ navigation }) {
 return (
 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 <Text>Test Screen</Text>
-<Button title="Go to Login" onPress={() => navigation.navigate('Login')} />
 <Button title="Go to Password Viewer" onPress={() => navigation.navigate('PasswordViewer')} />
 <Button title="Go Update User Details" onPress={() => navigation.navigate('UpdUserDetails')} />
 <Button title="Go to Generate Password" onPress={() => navigation.navigate('PasswordGen')} />
